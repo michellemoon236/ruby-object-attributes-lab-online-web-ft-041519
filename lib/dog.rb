42683207b4dc70b6d9c0c17be91a8edef439d856
Michelle Moon <michellemoon236@gmail.com>
2019-04-16 18:45:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,21 @@
 class Dog 
   def name=(dog_name)
-    @dog_name = dog_name
+    @name = dog_name
   end
   
   def name
-    @dog_name
+    @name
   end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  
+  
+  
 end 
