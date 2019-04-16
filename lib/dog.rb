f3063a2a2b5aaad8cdd4f71786e880091159cf79
Michelle Moon <michellemoon236@gmail.com>
2019-04-16 18:47:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(given_breed)
+    @breed = given_breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  
+  
+  
+end 
